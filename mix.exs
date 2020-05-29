5aeb7d0174764ad6a6cb2b23482e51346230ff8c
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2020-05-29 22:13:33 +0500

    [CI] Update the configuration and use ex_check

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule TypedStructLens.MixProject do
   defp deps do
     [
       # Development dependencies
+      {:ex_check, "~> 0.11.0", only: :dev, runtime: false},
       {:credo, "~> 1.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0-rc", only: :dev, runtime: false},
       {:excoveralls, ">= 0.0.0", only: :test, runtime: false},
